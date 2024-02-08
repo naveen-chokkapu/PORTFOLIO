d1f65e17aba5138f2afc7dea9688e1baea40ab44
naveen <chokkapunaveen260503@gmail.com>
2024-02-08 19:30:14 +0530

    edited

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 const About = () => {
   return (
     <div>
-      <div className="h-screen bg-gradient-to-r from-red-500/[0] to-violet-500/[0.2]">
+      <div className="h-fit bg-gradient-to-r from-red-500/[0] to-violet-500/[0.2]">
         <div className="">
           <Header />
         </div>
